d7c9dd3e58a985ff7758d6074859d7ee9d0adeec
Rohit Kumar <rohit97751@gmail.com>
2022-11-14 22:24:56 +0530

    recursion : factorial

---
+++ Algorithum/Code Help By Babbar/01_recursion/lec31/01_factorial.js
@@ -0,0 +1,12 @@
+const factNum = 5;
+
+function factorial(n) {
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(factNum));
+
+// 5! = 5 * (5 - 1)!
+// n! = n * (n - 1)!
+// f(n) = n * f(n -1)
