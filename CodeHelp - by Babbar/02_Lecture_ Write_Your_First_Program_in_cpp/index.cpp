c8b8d4ec575203c67a328b59f421d51df9edc030
Rohit Kumar <rohit96682@gmail.com>
2023-08-26 22:56:11 +0530

    Added CodeHelp - by Babbar folder

---
+++ CodeHelp - by Babbar/02_Lecture_ Write_Your_First_Program_in_cpp/index.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    int a = 'a';
+    cout << a << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+
+    int b = 2 / 5;
+    cout << b << endl;
+
+    float c = 2.0 / 5;
+    cout << c << endl;
+
+    double d = 2.0 / 5;
+    cout << d << endl;
+}
