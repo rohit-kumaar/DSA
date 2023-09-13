cf6c63c55ce84be4c6707aad2075efbe98ea1e0b
Rohit Kumar <rohit96682@gmail.com>
2023-09-13 22:45:22 +0530

    1991. Find the Middle Index in Array

---
+++ practice.js
@@ -0,0 +1,15 @@
+// 1991. Find the Middle Index in Array
+
+
+function middleIndex(arr) {
+  for (let i = 0; i <= arr.length - 1; i++) {
+    for (let j = 1; j <= arr.length - 1; j++) {
+      let rightValue = arr[j] + arr[j + 1];
+      console.log(rightValue);
+    }
+  }
+}
+
+const arr = [2, 3, -1, 8, 4];
+const result = middleIndex(arr);
+console.log(result);
