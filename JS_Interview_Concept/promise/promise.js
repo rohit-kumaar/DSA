976a62cd9870a07888fbafb8577df4bc233f368c
Rohit Kumar <rohit96682@gmail.com>
2023-10-07 19:38:43 +0530

    update promise

--- JS_Interview_Concept/promise/promise.js
+++ JS_Interview_Concept/promise/promise.js
@@ -1,21 +1,29 @@
 const URL = "https://jsonplaceholder.typicode.com/users";
 
 const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("Hello World");
-    resolve(50);
-  }, 2000);
+  let isCancelled = false;
+
+  if (!isCancelled) {
+    setTimeout(() => {
+      console.log("Hello World");
+      resolve("Your Promise is Resolve");
+    }, 2000);
+  } else {
+    reject("Your Promise is Reject");
+  }
 });
 
 // Fetch only work in browser
 async function main() {
   try {
     const [number, response] = await Promise.all([myPromise, fetch(URL)]);
-    console.log(number);
+    console.log("Number :", number);
     const data = await response.json();
-    console.log(data);
+    console.log("Data :", data);
   } catch (error) {
     console.log(error);
+  } finally {
+    console.log("Promise completed.");
   }
 }
 main();
