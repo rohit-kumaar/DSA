7b4679aff4dede6078edbfbe8d6fdb62575a2a51
Rohit Kumar <rohit96682@gmail.com>
2023-11-08 22:35:40 +0530

    remove output folder

---
+++ CodeHelp - by Babbar/12_ Binary_Search_Explained/04_peakIndexInAMountainArray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int peakIndexInAMountainArray(int arr[], int size)
+{
+    int startIdx = 0;
+    int endIdx = size - 1;
+    int midIdx = startIdx + (endIdx - startIdx) / 2;
+
+    while (startIdx < endIdx)
+    {
+        if (arr[midIdx] < arr[midIdx + 1])
+        {
+            startIdx = midIdx + 1;
+        }
+        else
+        {
+            endIdx = midIdx;
+        }
+
+        midIdx = startIdx + (endIdx - startIdx) / 2;
+    }
+
+    return startIdx;
+}
+
+int main()
+{
+    // int arr[] = {0, 2, 1, 0};
+    // int arr[] = {0,1,0};
+    int arr[] = {0, 1, 10, 5, 2};
+    cout << "Peak index in a mountain array is : " << peakIndexInAMountainArray(arr, 5) << endl;
+}
