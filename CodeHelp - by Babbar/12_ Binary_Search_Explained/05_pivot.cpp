f373197a014f1e54dd839cbeeb3aa3c18a516a2c
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2023-11-23 12:34:27 +0530

    pivot

---
+++ CodeHelp - by Babbar/12_ Binary_Search_Explained/05_pivot.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int pivot(int arr[], int size)
+{
+    int startIdx = 0;
+    int endIdx = size;
+    int midIdx = startIdx + (endIdx - startIdx) / 2;
+
+    while (startIdx < endIdx)
+    {
+        if (arr[midIdx] >= arr[0])
+        {
+            startIdx = midIdx + 1;
+        }
+        else
+        {
+            endIdx = midIdx;
+        }
+
+        midIdx = startIdx + (endIdx - startIdx) / 2;
+    }
+    return startIdx;
+}
+
+int main()
+{
+    int arr[] = {8, 10, 17, 1, 3};
+    cout << "Find pivot in an array : " << pivot(arr, 5) << endl;
+}
