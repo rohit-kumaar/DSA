a1bb9f552b19b2d34ff0294c0fa4a463660004f3
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-01-03 18:21:23 +0530

    update gitignore

--- CodeHelp - by Babbar/practice.cpp
+++ CodeHelp - by Babbar/practice.cpp
@@ -1,34 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int size, int key)
-{
-    int leftIndex = 0;
-    int rightIndex = size - 1;
-    int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
 
-    while (leftIndex <= rightIndex)
-    {
-        if (arr[middleIndex] == key)
-        {
-            return middleIndex;
-        }
-
-        if (key > arr[middleIndex])
-        {
-            leftIndex = middleIndex + 1;
-        }
-        else
-        {
-            rightIndex = middleIndex - 1;
-        }
-        middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
-    }
-    return -1;
-}
 
 int main()
 {
-    int arr[6] = {2, 3, 4, 5, 6, 7};
-    cout << "Index of 20 is : " << binarySearch(arr, 6, 20) << endl;
+   cout<<"Enter"
 }
