aa627f9f0109585924a49867894574090ffc6bca
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-01-04 16:03:00 +0530

    update 01

--- CodeHelp - by Babbar/04_Solving_Pattern_Questions_Part-2/01.cpp
+++ CodeHelp - by Babbar/04_Solving_Pattern_Questions_Part-2/01.cpp
@@ -11,21 +11,22 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int num;
+    cout << "Enter the number : ";
+    cin >> num;
 
-    int i = 1;
-    while (i <= n)
+    int row = 1;
+
+    while (row <= num)
     {
+        int col = 1;
 
-        int j = 1;
-        while (j <= n)
+        while (col <= num)
         {
-            cout << j << " ";
-            j = j + 1;
+            cout << col << " ";
+            col++;
         }
         cout << endl;
-
-        i = i + 1;
+        row++;
     }
 }
--- CodeHelp - by Babbar/practice.cpp
+++ CodeHelp - by Babbar/practice.cpp
@@ -3,5 +3,23 @@ using namespace std;
 
 int main()
 {
-  
+
+    int num;
+    cout << "Enter the number : ";
+    cin >> num;
+
+    int row = 1;
+
+    while (row <= num)
+    {
+        int col = 1;
+
+        while (col <= num)
+        {
+            cout << col << " ";
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
 }
