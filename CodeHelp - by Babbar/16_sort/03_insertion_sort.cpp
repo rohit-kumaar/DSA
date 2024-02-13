684bdc70bb10eeaac63f48fbd338bcc7b2404456
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-02-13 13:04:49 +0530

    insertion sort

---
+++ CodeHelp - by Babbar/16_sort/03_insertion_sort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void printArr(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void insertionSort(int arr[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        for (; j >= 0; j--)
+        {
+            if (arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 1, 7, 4, 8, 2, 11};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, size);
+    printArr(arr, size);
+}
