db816adf757f5e69e0c756a3edb8ed2252e972b4
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-02-22 10:44:00 +0530

    update : rotate array

--- CodeHelp - by Babbar/21_solving_questions_array/01_rotate_array.cpp
+++ CodeHelp - by Babbar/21_solving_questions_array/01_rotate_array.cpp
@@ -10,11 +10,18 @@ void printArr(int arr[], int size)
     cout << endl;
 }
 
-void rotateArr(int arr[], int size, int steps, int result[])
+void rotateArr(int arr[], int size, int rotateFrom, int rotatedArr[])
 {
     for (int i = 0; i < size; i++)
     {
-        result[(i + steps) % size] = arr[i];
+        // result[(0 + 3) % 7] = arr[0];
+        // result[(1 + 3) % 7] = arr[1];
+        // result[(2 + 3) % 7] = arr[2];
+        // result[(3 + 3) % 7] = arr[3];
+        // result[(4 + 3) % 7] = arr[4];
+        // result[(5 + 3) % 7] = arr[5];
+        // result[(6 + 3) % 7] = arr[6];
+        rotatedArr[(i + rotateFrom) % size] = arr[i];
     }
 }
 
@@ -22,11 +29,9 @@ int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6, 7};
     int size = sizeof(arr) / sizeof(arr[0]);
-    int steps = 3;
-    int rotatedArr[size];
+    int rotateFrom = 3;
+    int rotatedArr[size] = {};
 
-    rotateArr(arr, size, steps, rotatedArr);
+    rotateArr(arr, size, rotateFrom, rotatedArr);
     printArr(rotatedArr, size);
-
-    return 0;
 }
