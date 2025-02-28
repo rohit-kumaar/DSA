5887120e09137fee49343cfdf65535a44a89a69b
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-02-28 17:35:28 +0530

    update

--- KodingKevin/07_capitalize.js
+++ KodingKevin/07_capitalize.js
@@ -1,22 +1,10 @@
-// Write a function that returns the provided string with
-// the first letter of each word capitalized. Make sure the
-// rest of the word is in lower case.
-// Examples
-// 11 capitalize("I'm a little tea pot")> 'I'm A Little Tea Pot'
-// capitalize('sHoRt AnD sTout')-> 'Short And Stout
+const words = "I'm a little tea pot";
 
-let words = "I'm a little tea pot";
-
-const capitalize = (str) => {
-  const arrOfWords = str.split(" ");
-  const arrOfWordsCased = [];
-
-  for (let i = 0; i < arrOfWords.length; i++) {
-    const word = arrOfWords[i];
-    arrOfWordsCased.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
-  }
-
-  return arrOfWordsCased.join(" ");
-};
+function capitalize(word) {
+  return word
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
 
 console.log(capitalize(words));
