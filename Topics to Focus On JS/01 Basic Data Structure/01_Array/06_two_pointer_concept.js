a2373daa0ee18f8738a4dbb2b643a07123ec5841
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-03-18 15:54:22 +0530

    🎨

--- Topics to Focus On JS/01 Basic Data Structure/01_Array/06_two_pointer_concept.js
+++ Topics to Focus On JS/01 Basic Data Structure/01_Array/06_two_pointer_concept.js
@@ -1,18 +1,13 @@
 const arr = [1, 1, 0, 0, 0, 0, 1, 0];
 
-function sortArr(arr) {
-  let i = 0;
-  let j = arr.length - 1;
-
+function sortArr(arr, i = 0, j = arr.length - 1) {
   while (i < j) {
     while (arr[i] === 0 && i < j) i++;
 
     while (arr[j] === 1 && i < j) j--;
 
     if (i < j) {
-      let temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
       i++;
       j--;
     }
