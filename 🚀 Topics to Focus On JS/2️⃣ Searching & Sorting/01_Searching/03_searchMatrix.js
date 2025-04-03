231afd9c6c7d06430664b66b83b7617cdc5c6798
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-04-03 14:59:11 +0530

    🚧  Add searchMatrix function to perform binary search on a 2D matrix

---
+++ 🚀 Topics to Focus On JS/2️⃣ Searching & Sorting/01_Searching/03_searchMatrix.js
@@ -0,0 +1,28 @@
+function searchMatrix(matrix, target) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  console.log(`${m} X ${n}`);
+  let start = 0;
+  let end = m * n - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    let i = Math.floor(mid / n);
+    let j = mid % n;
+
+    let midNum = matrix[i][j];
+
+    if (target === midNum) return `ith row: ${i} and jth column: ${j}`;
+    target > midNum ? (start = mid + 1) : (end = mid - 1);
+  }
+  
+}
+
+const matrix = [
+  [1, 3, 5, 7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 60],
+];
+const target = 3;
+const res = searchMatrix(matrix, target);
+console.log(res);
