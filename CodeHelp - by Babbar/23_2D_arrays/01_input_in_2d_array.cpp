0b7aea74b27468161e024def0f7c8902bb73eb22
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-04-04 12:19:58 +0530

    ✨  Initialize 2D array with predefined values and comment out user input section

--- CodeHelp - by Babbar/23_2D_arrays/01_input_in_2d_array.cpp
+++ CodeHelp - by Babbar/23_2D_arrays/01_input_in_2d_array.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main()
 {
 
-    int arr[3][4];
+    int arr[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
     // row wish input
-    for (int row = 0; row < 3; row++)
-    {
-        for (int col = 0; col < 4; col++)
-        {
-            cin >> arr[row][col];
-        }
-    }
+    // for (int row = 0; row < 3; row++)
+    // {
+    //     for (int col = 0; col < 4; col++)
+    //     {
+    //         cin >> arr[row][col];
+    //     }
+    // }
 
     // column wish input
     // for (int col = 0; col < 4; col++)
