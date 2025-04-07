6248fb7eae1328631fde5504c5b7e66aed89fb95
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-04-07 16:54:34 +0530

    ✨ Add implementations for 2D array operations: row-wise input, column-wise input, initialization, linear search, largest row sum, and wave print.

---
+++ CodeHelp - by Babbar/23_2D_arrays/01_print_row_wise.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[3][4];
+
+    // row wish input
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cin >> arr[row][col];
+        }
+    }
+
+    // print
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cout << arr[row][col] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
--- CodeHelp - by Babbar/23_2D_arrays/02_print_col_wise.cpp
+++ CodeHelp - by Babbar/23_2D_arrays/02_print_col_wise.cpp
@@ -4,17 +4,7 @@ using namespace std;
 int main()
 {
 
-    // int arr[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-
-    // row wish input
-    // for (int row = 0; row < 3; row++)
-    // {
-    //     for (int col = 0; col < 4; col++)
-    //     {
-    //         cin >> arr[row][col];
-    //     }
-    // }
+    int arr[3][4];
 
     // column wish input
     for (int col = 0; col < 4; col++)
