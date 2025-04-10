24cfcc9338f7e10b4aa50aa12c927d490ba344c5
Rohit Kumar <rohit96682@gmail.com>
2025-04-10 20:12:04 +0530

    🐛  Refactor printInCol function to accept dynamic row and column parameters

--- Topics_to_Focus_On_JS/[ 01 ] Basic Data Structure/01_Array/03_Matrix/02_print_col_wise.js
+++ Topics_to_Focus_On_JS/[ 01 ] Basic Data Structure/01_Array/03_Matrix/02_print_col_wise.js
@@ -1,14 +1,10 @@
-function printInCol(arr) {
-  const row = 3;
-  const col = 3;
-  const t = Math.floor(Math.sqrt(arr.length));
-  const matrix = Array.from({ length: t }, () => new Array(t).fill(0));
-  let count = 0;
+function printInCol(arr, row, col) {
+  const matrix = Array.from({ length: row }, () => new Array(col).fill(0));
+  let idx = 0;
 
   for (let n = 0; n < col; n++) {
     for (let m = 0; m < row; m++) {
-      matrix[m][n] = arr[count];
-      count++;
+      matrix[m][n] = arr[idx++];
     }
   }
 
@@ -16,5 +12,7 @@ function printInCol(arr) {
 }
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const res = printInCol(arr);
+const row = 3;
+const col = 3;
+const res = printInCol(arr, row, col);
 console.log(res);
