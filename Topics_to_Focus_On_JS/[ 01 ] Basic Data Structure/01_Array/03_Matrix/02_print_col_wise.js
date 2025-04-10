c037665d241a0107b362b9daca7466e86becdec3
Rohit Kumar <rohit96682@gmail.com>
2025-04-10 12:28:10 +0530

    ✨ Add printInRow function to output a 2D matrix in row-wise and column-wise order

---
+++ Topics_to_Focus_On_JS/[ 01 ] Basic Data Structure/01_Array/03_Matrix/02_print_col_wise.js
@@ -0,0 +1,20 @@
+function printInRow(arr) {
+  const row = 3;
+  const col = 3;
+  const t = Math.floor(Math.sqrt(arr.length));
+  const matrix = Array.from({ length: t }, () => new Array(t).fill(0));
+  let count = 0;
+
+  for (let n = 0; n < col; n++) {
+    for (let m = 0; m < row; m++) {
+      matrix[m][n] = arr[count];
+      count++;
+    }
+  }
+
+  return matrix;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const res = printInRow(arr);
+console.log(res);
