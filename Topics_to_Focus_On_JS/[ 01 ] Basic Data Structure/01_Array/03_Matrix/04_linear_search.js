f22c5f7ccebd80e867a4f3e8fc5bce69c28769c9
Rohit Kumar <rohit96682@gmail.com>
2025-04-10 12:58:15 +0530

    ✨ Implement linear search algorithm with matrix representation

---
+++ Topics_to_Focus_On_JS/[ 01 ] Basic Data Structure/01_Array/03_Matrix/04_linear_search.js
@@ -0,0 +1,34 @@
+function isPresent(matrix, row, col, target) {
+  for (let m = 0; m < row; m++) {
+    for (let n = 0; n < col; n++) {
+      if (matrix[m][n] === target) {
+        return 1;
+      }
+    }
+  }
+
+  return 0;
+}
+
+function linearSearch(arr) {
+  const row = 3;
+  const col = 3;
+  const t = Math.floor(Math.sqrt(arr.length));
+  const matrix = Array.from({ length: t }, () => new Array(t).fill(0));
+  let count = 0;
+
+  for (let m = 0; m < row; m++) {
+    for (let n = 0; n < col; n++) {
+      matrix[m][n] = arr[count];
+      count++;
+    }
+  }
+
+  const target = 4;
+
+  return isPresent(matrix, row, col, target) ? "Element Found" : "Not Found";
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const res = linearSearch(arr);
+console.log(res);
