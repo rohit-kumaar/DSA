71c4d2a30f4f39dd03ccb7f8cd8b58d2cf4ba0f2
Rohit Kumar <rohit96682@gmail.com>
2025-04-10 20:07:09 +0530

    ✨ Refactor wavePrint function to fill and print a 2D matrix in wave order

--- Code_Help_By_Babbar/23_2D_arrays/06_wave_print_problem.cpp
+++ Code_Help_By_Babbar/23_2D_arrays/06_wave_print_problem.cpp
@@ -1,42 +1,52 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+void wavePrint(vector<int> &arr, int row, int col)
+{
+    // Create a 2D matrix
+    int matrix[3][3]; // Assuming max size 3 for simplicity
+    int idx = 0;
+
+    // Fill the matrix column by column
+    for (int n = 0; n < col; n++)
+    {
+        if ((n & 1) == 1)
+        {
+            // If the column is odd, go bottom to top
+            for (int m = row - 1; m >= 0; m--)
+            {
+                matrix[m][n] = arr[idx++];
+            }
+        }
+        else
+        {
+            // If the column is even, go top to bottom
+            for (int m = 0; m < row; m++)
+            {
+                matrix[m][n] = arr[idx++];
+            }
+        }
+    }
+
+    // Print the matrix
+    for (int m = 0; m < row; m++)
+    {
+        for (int n = 0; n < col; n++)
+        {
+            cout << matrix[m][n] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
-    int arr[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int row = 3;
+    int col = 3;
 
-    for (int j = 0; j < 3; j++)
-    {
-
-        // if (j % 2 == 0)
-        // {
-        for (int i = 0; i < 3; i++)
-        {
-
-            cout << arr[i][j] << " ";
-        }
-        // }
-
-        // if (j % 2 == 0)
-        // {
-        //     // Traverse top to bottom
-        //     for (int i = 0; i < 3; i++)
-        //     {
-
-        //         cout << arr[i][j] << " ";
-        //     }
-        // }
-        // else
-        // {
-        //     // Traverse from bottom to top
-        //     for (int i = 2; i >= 0; i--)
-        //     {
-        //         cout << arr[i][j] << " ";
-        //     }
-        // }
-
-        cout << endl;
-    }
+    wavePrint(arr, row, col);
 
     return 0;
 }
