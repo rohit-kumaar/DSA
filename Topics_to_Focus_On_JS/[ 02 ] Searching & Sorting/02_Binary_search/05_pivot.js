c5eec3046f9a9e2c6f07372f43d0cc34f7d070f0
Rohit Kumar <rohit96682@gmail.com>
2025-04-12 10:50:38 +0530

    ✨ Implement pivot function to find the pivot index in a rotated sorted array

---
+++ Topics_to_Focus_On_JS/[ 02 ] Searching & Sorting/02_Binary_search/05_pivot.js
@@ -0,0 +1,20 @@
+function pivot(arr) {
+  let startIdx = 0;
+  let endIdx = arr.length;
+  let midIdx = Math.floor((startIdx + endIdx) / 2);
+
+  while (startIdx < endIdx) {
+    if (arr[midIdx] >= arr[0]) {
+      startIdx = midIdx + 1;
+    } else {
+      endIdx = midIdx;
+    }
+    midIdx = Math.floor((startIdx + endIdx) / 2);
+  }
+
+  return arr[midIdx];
+}
+
+const arr = [6, 7, 8, 9, 1, 2, 3, 4, 5];
+const res = pivot(arr);
+console.log(res);
