4404359d996d161f6dbe34c48833b74dac4c9fbf
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-04-21 13:02:19 +0530

    🐛 Refactor maxProfit function to improve variable naming and logic for calculating profit

--- Topics_to_Focus_On_JS/[ 15 ] Dynamic Programming/07_best_time_to_buy_&_sell_stock.js
+++ Topics_to_Focus_On_JS/[ 15 ] Dynamic Programming/07_best_time_to_buy_&_sell_stock.js
@@ -1,16 +1,18 @@
 function maxProfit(price) {
   if (!price || price.length < 2) return 0;
 
-  let minPrice = price[0];
-  let maxProfit = 0;
+  let buyPrice = price[0];
+  let profit = 0;
 
   for (let i = 1; i < price.length; i++) {
-    let currProfit = price[i] - minPrice;
-    maxProfit = Math.max(maxProfit, currProfit);
-    minPrice = Math.min(minPrice, price[i]);
+    let sellPrice = price[i];
+    if (buyPrice > sellPrice) {
+      buyPrice = sellPrice;
+    }
+    profit = Math.max(profit, sellPrice - buyPrice);
   }
 
-  return maxProfit;
+  return profit;
 }
 
 const price = [7, 1, 5, 3, 6, 4];
