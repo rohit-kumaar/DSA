1411a5e2940ffd5bbfa26afd5f38851d0f2c8fd2
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-04-21 17:30:01 +0530

    ✅ Implement binary tree creation and level order traversal

---
+++ Code_Help_By_Babbar/62_Binary_Trees/01_binary_tree.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node *buildTree(node *root)
+{
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+
+    if (data == -1)
+    {
+        return NULL;
+    };
+
+    root = new node(data);
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+
+    return root;
+};
+
+void levelOrderTraversal(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        cout << temp->data << " ";
+        q.pop();
+
+        if (temp->left)
+        {
+            q.push(temp->left);
+        };
+
+        if (temp->right)
+        {
+            q.push(temp->right);
+        };
+    };
+};
+
+int main()
+{
+    node *root = NULL;
+
+    // create a tree
+    root = buildTree(root);
+
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    // level order
+    cout << "Printing the level order traversal output " << endl;
+    levelOrderTraversal(root);
+
+    return 0;
+}
