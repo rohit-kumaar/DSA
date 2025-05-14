c208b87401ec8e42f3327cc02a19144ce3ea975d
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-05-14 13:12:29 +0530

    ✨ Refactor threeSum function for improved readability and consistency

--- Topics_to_Focus_On_JS/[ 07 ] Two Pointer/02_threeSum.js
+++ Topics_to_Focus_On_JS/[ 07 ] Two Pointer/02_threeSum.js
@@ -1,33 +1,32 @@
 function threeSum(a) {
   const arr = a.sort((a, b) => a - b);
-  const len = arr.length;
   const answer = [];
 
-  for (let i = 0; i < len; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] > 0) break;
     if (i > 0 && arr[i] === arr[i - 1]) continue;
 
     let lo = i + 1;
-    let hi = len - 1;
+    let hi = arr.length - 1;
 
     while (lo < hi) {
       let sum = arr[i] + arr[lo] + arr[hi];
       if (sum === 0) {
         answer.push([arr[i], arr[lo], arr[hi]]);
-        lo = lo + 1;
-        hi = hi - 1;
+        lo++;
+        hi--;
 
         while (lo < hi && arr[lo] === arr[lo - 1]) {
-          lo = lo + 1;
+          lo++;
         }
 
         while (lo < hi && arr[hi] === arr[hi + 1]) {
-          hi = hi - 1;
+          hi--;
         }
       } else if (sum < 0) {
-        lo = lo + 1;
+        lo++;
       } else {
-        hi = hi - 1;
+        hi--;
       }
     }
   }
