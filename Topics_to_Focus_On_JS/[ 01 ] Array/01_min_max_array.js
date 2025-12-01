936872f00c8c866743d860f053613d48c6579b8a
Rohit Kumar <94822153+rohit-kumaar@users.noreply.github.com>
2025-12-01 13:50:33 +0530

    📈 Up to reverse str

--- Topics_to_Focus_On_JS/[ 01 ] Array/01_min_max_array.js
+++ Topics_to_Focus_On_JS/[ 01 ] Array/01_min_max_array.js
@@ -1,27 +1,18 @@
-function getMin(arr) {
-  if (arr.length === 0) return null;
+function getMinMax(arr) {
+  if (arr.length === 0) return { min: null, max: null };
 
-  let minNum = Infinity;
-  for (let i = 0; i < arr.length; i++) {
-    minNum = Math.min(minNum, arr[i]);
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
   }
 
-  return minNum;
+  return { min, max };
 }
 
-function getMax(arr) {
-  if (arr.length === 0) return null;
-
-  let maxNum = -Infinity;
-  for (let i = 0; i < arr.length; i++) {
-    maxNum = Math.max(maxNum, arr[i]);
-  }
-
-  return maxNum;
-}
-
-// const arr = [];
 const arr = [34, 7, 23, 32, 5, 62];
 
-console.log("Minimum value is:", getMin(arr));
-console.log("Maximum value is:", getMax(arr));
+const result = getMinMax(arr);
+console.log(result);
--- Topics_to_Focus_On_JS/[ 01 ] Array/02_sum_of_array.js
+++ Topics_to_Focus_On_JS/[ 01 ] Array/02_sum_of_array.js
@@ -1,13 +1,7 @@
-const arr = [1, 2, 3, 4, 5];
-
-function sumOfArray(arr) {
-  let ans = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    ans = ans + arr[i];
-  }
-
-  return ans;
+function sumOfArray(arr, i = 0) {
+  if (i >= arr.length) return 0;
+  return arr[i] + sumOfArray(arr, i + 1);
 }
 
+const arr = [1, 2, 3, 4, 5];
 console.log(sumOfArray(arr));
