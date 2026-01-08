1fb887b688164c99b5c679b7e2cd3a1fb3904d9f
Rohit Kumar <94822153+rohit-kumaar@users.noreply.github.com>
2026-01-08 14:59:58 +0530

    ✅Statement

--- JS_Interview_Concept/20_class.js
+++ JS_Interview_Concept/20_class.js
@@ -1,19 +1,24 @@
+/*
+A class is the blueprint for creating objects, and the constructor is a special method that initializes those objects when they are created.
+*/
+
 class Developer {
-    constructor(name, skills) {
-        this.name = name;
-        this.skills = skills;
-    }
+  constructor(name, skills) {
+    this.name = name;
+    this.skills = skills;
+  }
 
-    /* --------------------------------- */
-    /* START : non enumerable property   */
-    /* --------------------------------- */
-    introduce() {
-        console.log(`Hi, I'm ${this.name} and I specialize in ${this.skills.join(", ")}.`);
-    }
-    /* --------------------------------- */
-    /* END   : non enumerable property   */
-    /* --------------------------------- */
-
+  /* --------------------------------- */
+  /* START : non enumerable property   */
+  /* --------------------------------- */
+  introduce() {
+    console.log(
+      `Hi, I'm ${this.name} and I specialize in ${this.skills.join(", ")}.`
+    );
+  }
+  /* --------------------------------- */
+  /* END   : non enumerable property   */
+  /* --------------------------------- */
 }
 
 // Creating an instance (object)
@@ -25,11 +30,12 @@ dev1.introduce();
 /* START : enumerable property   */
 /* ----------------------------- */
 Developer.prototype.enumerableProp = function () {
-    console.log(`Hi, I'm ${this.name} and I specialize in ${this.skills.join(", ")}.`);
-}
+  console.log(
+    `Hi, I'm ${this.name} and I specialize in ${this.skills.join(", ")}.`
+  );
+};
 
 console.log(dev1.enumerableProp());
-
 
 /* ----------------------------- */
 /* END   : enumerable property   */
@@ -39,14 +45,14 @@ console.log(dev1.enumerableProp());
 /* START : extends developer property in to backend developer */
 /* ---------------------------------------------------------- */
 class BackendDeveloper extends Developer {
-    developAPI() {
-        console.log(`${this.name} is building an API.`);
-    }
+  developAPI() {
+    console.log(`${this.name} is building an API.`);
+  }
 }
 
 const dev2 = new BackendDeveloper("Amit", ["Node.js", "MongoDB"]);
-dev2.introduce();   // Inherited method
-dev2.developAPI();  // Output: Amit is building an API.
+dev2.introduce(); // Inherited method
+dev2.developAPI(); // Output: Amit is building an API.
 
 /* ---------------------------------------------------------- */
 /* END   : extends developer property in to backend developer */
