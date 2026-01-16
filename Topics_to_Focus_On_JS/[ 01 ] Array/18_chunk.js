a66111ac57b9f348b881c8d02fb0eddd170236bf
Rohit Kumar <94822153+rohit-kumaar@users.noreply.github.com>
2026-01-16 15:15:43 +0530

    ✅ simplified

--- Topics_to_Focus_On_JS/[ 01 ] Array/18_chunk.js
+++ Topics_to_Focus_On_JS/[ 01 ] Array/18_chunk.js
@@ -1,29 +1,14 @@
-// - Directions
-// Write a function that splits an array (first argument)
-// into groups the length of size (second argument) and
-// returns them as a two-dimensional array.
-// Examples
-// chunk ( ["a", "b", "c", "d"], 2) [[ "a", "b"], ["c", "d"]]
-// chunk ( [0, 1, 2, 3, 4, 5], 4) [[0, 1, 2, 3], [4, 5]]
+const arr = ["a", "b", "c", "d", "e"];
+const chunk = [];
+const size = 2;
 
-const arr = ["a", "b", "c", "d"];
-const arrChunkSize = 2;
-
-const chunk = (array, size) => {
-  const result = [];
-
-  for (let i = 0; i < array.length; i++) {
-    const item = array[i];
-    const last = result[result.length - 1];
-
-    if (!last || last.length === size) {
-      result.push([item]);
-    } else {
-      last.push(item);
-    }
+function chunkFn(arr, size) {
+  for (let i = 0; i < arr.length; i = i + size) {
+    chunk.push(arr.slice(i, i + size));
   }
 
-  return result;
-};
+  return chunk;
+}
 
-console.log(chunk(arr, arrChunkSize));
+const res = chunkFn(arr, size);
+console.log(res);
